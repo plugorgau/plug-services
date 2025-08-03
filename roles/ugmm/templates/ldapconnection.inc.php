5ba523843d1d623537d771b1cd8a2b54b1469e3a
James Henstridge <james@jamesh.id.au>
2025-08-04 07:20:43 +0800

    roles: move ugmm task to a role, installing deb from Github releases

---
+++ roles/ugmm/templates/ldapconnection.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+// {{ ansible_managed }}
+
+// Copy this to /etc/private/ldapconnection.inc.php and modify it to suit your ldap database
+
+require_once 'Net/LDAP2.php';
+    // The configuration array:
+    $config = array (
+        'binddn'    => 'cn=admin,dc=plug,dc=org,dc=au',
+        'bindpw'    => '{{ ldap_admin_passphrase }}',
+        'basedn'    => 'dc=plug,dc=org,dc=au',
+        'host'      => 'ldapi:///'
+    );
+
+    // Connecting using the configuration:
+    $ldap = Net_LDAP2::connect($config);
+
+    // Testing for connection error
+    if (PEAR::isError($ldap)) {
+        die('Could not connect to LDAP-server: '.$ldap->getMessage());
+    }
